33dba92da0fb5534c4f64c3fc0623aa44f6c1892
rvquichimbo <ronal1923@gmail.com>
2021-12-07 22:03:58 -0500

    CI-CD

---
+++ api_rest/products.py
@@ -0,0 +1,7 @@
+products= [
+    {"name":"laptop", "price":800, "quantity":4},
+    {"name":"laptop", "price":900, "quantity":8},
+    {"name":"mouse", "price":850, "quantity":14},
+    {"name":"Pc", "price":1800, "quantity":7},
+    {"name":"Teclado", "price":900, "quantity":8},
+]
